5f93b5b269f7e9fbdb627bdc32849ee8e733185f
bruce-mig <brucetmig@icloud.com>
2025-04-24 19:56:37 +0200

    chore: added graph header file

--- graph.h
+++ graph.h
@@ -14,10 +14,11 @@
  *         Author:  BRUCE MIGERI, Networking Developer (BM), bmigeri@gmail.com
  *
  *        This file is part of the bruce-mig distribution
- * 		(https://github.com/bruce-mig). Copyright (c) 2025 Bruce Migeri. This
- *			program is free software: you can redistribute it and/or modify it
- *			under the terms of the GNU General Public License as published by the
- *			Free Software Foundation, version 3.
+ *        (https://github.com/bruce-mig).
+ *        Copyright (c) 2025 Bruce Migeri.
+ *        This program is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU General Public License as published by
+ *        the Free Software Foundation, version 3.
  *
  *        This program is distributed in the hope that it will be useful, but
  *        WITHOUT ANY WARRANTY; without even the implied warranty of
